6e5f66b7785604d33713b6523e5d052a13578b35
StasAbrosimov <stasabrosimov@gmail.com>
2020-01-23 17:44:23 +0200

    no message

--- CoreMVC/WebCoreMVC/WebCoreMVC/Controllers/EntityController.cs
+++ CoreMVC/WebCoreMVC/WebCoreMVC/Controllers/EntityController.cs
@@ -33,5 +33,70 @@ namespace WebCoreMVC.Controllers
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Update(user);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == id);
+                if (user != null)
+                    return View(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm]int? DeleteId)
+        {
+            if (ModelState.IsValid && DeleteId != null)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(p => p.Id == DeleteId);
+                if (user != null)
+                {
+                    db.Users.Remove(user);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
+        }
     }
 }
